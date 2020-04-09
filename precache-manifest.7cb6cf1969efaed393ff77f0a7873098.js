da04701ac46b9adfaaa64582c9760c41a4773d7b
Alexa Javellana <AlexaJ@Alexas-MacBook-Pro-2.local>
2020-04-09 11:59:11 +0900

    Updates

---
+++ precache-manifest.7cb6cf1969efaed393ff77f0a7873098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4010ebc3fe339dd8a55101ec4b36c72",
+    "url": "/Situationship/index.html"
+  },
+  {
+    "revision": "b3a8e8f5e80567d373fe",
+    "url": "/Situationship/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4e8a5a63f94ddefd0b2a",
+    "url": "/Situationship/static/js/2.030cb5e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Situationship/static/js/2.030cb5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a8e8f5e80567d373fe",
+    "url": "/Situationship/static/js/main.b296c4c2.chunk.js"
+  },
+  {
+    "revision": "312ace27cba4f40a0c51",
+    "url": "/Situationship/static/js/runtime-main.8151e26b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Situationship/static/media/logo.5d5d9eef.svg"
+  }
+]);
